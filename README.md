be2e6856aabd0995ddfbe9c8610f2053f50b582d
Nepcam <cameron.nepe@gmail.com>
2019-12-07 21:35:39 +1300

    iterators and generators

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # js-sandbox-17
 Newer Features - ES2015+
+
+1. Iterators and Generators
---
+++ app.js
@@ -0,0 +1,19 @@
+// Iterator Example
+function nameIterator(names) {
+  let nextIndex = 0;
+
+  return {
+    next: function() {
+      return nextIndex < names.length ?
+      { value: names[nextIndex++], done: false } :
+      { done: true }
+    }
+  }
+}
+
+// Create an array of names
+const namesArr = ['Mason', 'Darius', 'Liam'];
+// Init iterator and pass in the names array
+const names = nameIterator(namesArr);
+
+console.log(names.next().value);
