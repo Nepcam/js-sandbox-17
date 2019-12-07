1f76c787ffe8996b8798aded14dd4ab891987735
Nepcam <cameron.nepe@gmail.com>
2019-12-07 21:50:00 +1300

    generator example

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 // Iterator Example
-function nameIterator(names) {
-  let nextIndex = 0;
+// function nameIterator(names) {
+//   let nextIndex = 0;
 
-  return {
-    next: function() {
-      return nextIndex < names.length ?
-      { value: names[nextIndex++], done: false } :
-      { done: true }
-    }
-  }
+//   return {
+//     next: function() {
+//       return nextIndex < names.length ?
+//       { value: names[nextIndex++], done: false } :
+//       { done: true }
+//     }
+//   }
+// }
+
+// // Create an array of names
+// const namesArr = ['Mason', 'Darius', 'Liam'];
+// // Init iterator and pass in the names array
+// const names = nameIterator(namesArr);
+
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next());
+
+
+// Generator Example
+function* sayNames() {
+  yield 'Mason';
+  yield 'Darius';
+  yield 'Liam';
 }
 
-// Create an array of names
-const namesArr = ['Mason', 'Darius', 'Liam'];
-// Init iterator and pass in the names array
-const names = nameIterator(namesArr);
+const name = sayNames();
 
-console.log(names.next().value);
+console.log(name.next().value);
+console.log(name.next().value);
+console.log(name.next().value);
+console.log(name.next());
